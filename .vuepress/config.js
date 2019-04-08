30803bf119aa43231363c87ecec493db328a9e5f
Joe Fu <fuzeqi@gmail.com>
2019-04-08 16:46:29 +0930

    update

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -2,3 +2,12 @@ module.exports = {
   title: 'Hello VuePress',
   description: 'Just playing around'
 }
+
+module.exports = {
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/WinePlus服务器的架设.html' }
+    ]
+  }
+}
